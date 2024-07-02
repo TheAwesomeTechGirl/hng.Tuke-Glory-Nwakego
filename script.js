62b6af20d85bf48cdd60ba6c2e75ed912b44b974
Tuke-Glory-Nwakego <t.nwakego@kindlegatefoundation.org>
2024-07-02 17:00:05 +0100

    fixed Js error

--- script.js
+++ script.js
@@ -3,23 +3,6 @@ const currentTimeElem = document.getElementById("currentTime");
 const currentDayElem = document.getElementById("currentDay");
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    
-  
-    function updateTime() {
-      const now = new Date();
-      const utcString = now.toUTCString();
-      currentTimeElem.textContent = utcString.slice(17, 25); // Extract time part
-  
-      const dayOfWeek = now.toLocaleString("en-US", { weekday: "long" });
-      currentDayElem.textContent = dayOfWeek;
-    }
-  
-    updateTime();
-    setInterval(updateTime, 1000); // Update time every second
-  });
-  const currentTimeElem = document.getElementById("currentTime");
-  const currentDayElem = document.getElementById("currentDay");
 
   function updateTime() {
     const now = new Date();
